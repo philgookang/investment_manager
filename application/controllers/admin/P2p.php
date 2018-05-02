c3ab9cb643f00bad18afa9a6fa3fab7345f21837
Phil Goo Kang <philgookang@gmail.com>
2018-05-02 13:31:16 +0900

    added support for lease management

--- application/controllers/admin/P2p.php
+++ application/controllers/admin/P2p.php
@@ -21,10 +21,26 @@ class P2p extends CI_Controller {
 
     public function manage($idx = '-1') {
 
+		$rurl = '';
+		$company_type = $this->input->get('investment_type');
+
         $data = array();
         $data['product'] 		= ProductM::new()->setIdx($idx)->get();
+		if ($data['product']->getIdx()!=null) {
+			$company_type = $data['product']->getInvestmentType();
+		}
+		if ($company_type == PRODUCT_INVESTMENT_TYPE::STOCK) {
+			$rurl = '';
+			$company_type = COMPANY_TYPE::STOCK;
+		} else if ($company_type == PRODUCT_INVESTMENT_TYPE::P2PFUND) {
+			$rurl = '/admin/p2p/?investment_status=1';
+			$company_type = COMPANY_TYPE::FUNDING;
+		} else if ($company_type == PRODUCT_INVESTMENT_TYPE::LEASE) {
+			$rurl = '/admin/lease/';
+			$company_type = COMPANY_TYPE::LEASE;
+		}
 		$data['return_list'] 	= ReturnsM::new()->setProductIdx($idx)->getList();
-		$data['company_list'] 	= CompanyM::new()->setType(COMPANY_TYPE::FUNDING)->getList();
+		$data['company_list'] 	= CompanyM::new()->setType($company_type)->getList();
 
         $this->load->view('admin/template/head');
         $this->load->view('admin/page/p2p_manage', $data);
