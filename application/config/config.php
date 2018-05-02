def36ff04f1e7b1244af63cf0088116adb5d9c59
Phil Goo Kang <philgookang@gmail.com>
2018-05-02 13:30:45 +0900

    changed the base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://p2p.philgookang.com/';
+$config['base_url'] = 'http://investment.philgookang.com/';
 
 /*
 |--------------------------------------------------------------------------
